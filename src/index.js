3f649fae54c341c7adc7f47271cb9c5c496ceda4
Ekkapap Sricharoen <neopalm01@hotmail.com>
2021-02-15 14:27:09 +0700

    แก้ไขให้สามารถใช้ sub directory ได้

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={"/everywhere30"}>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
