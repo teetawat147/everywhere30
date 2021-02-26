1f2611a232c6a6598615f2f49697f82b263b8882
Ekkapap Sricharoen <neopalm01@hotmail.com>
2021-02-27 04:47:11 +0700

    แก้ bug

--- src/components/Consent.js
+++ src/components/Consent.js
@@ -24,7 +24,6 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import { useTheme } from '@material-ui/core/styles';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
