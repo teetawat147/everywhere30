d88c2d0f3dc0427ebb0167f018b4ad35e50247eb
Ekkapap Sricharoen <neopalm01@hotmail.com>
2021-03-04 22:45:41 +0700

    แก้ไข Validation และเพิ่ม Dialog

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,14 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useRef, useEffect } from "react";
-import { makeStyles } from '@material-ui/core';
+import {
+  Button,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  makeStyles
+} from "@material-ui/core";
+import { useDialog } from "../services/dialog/DialogProvider.tsx";
 import Form from "react-validation/build/form";
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import CheckButton from "react-validation/build/button";
-// import { isEmail } from "validator";
 import { useHistory } from "react-router-dom";
-import * as AuthService from "../services/auth.service";
+import { register } from "../services/auth.service";
 import { getAll } from "../services/UniversalAPI";
+import validation from "../services/validation";
 const useStyles = makeStyles(theme => ({
   root: {
     '& .MuiTextField-root': {
@@ -51,29 +57,21 @@ const Register = (props) => {
   const redirect = useHistory();
   const classes = useStyles();
   const form = useRef();
-  const checkBtn = useRef();
-  // const [fullname, setFullname] = useState('');
-  // const [position, setPosition] = useState('');
-  // const [cid, setCid] = useState('');
-  // const [mobile, setMobile] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
+  const [openDialog, closeDialog] = useDialog();
   const [changwats, setChangwats] = useState([]);
-  const [changwat, setChangwat] = useState('');
   const [departments, setDepartments] = useState([]);
-  const [department, setDepartment] = useState('');
-  const [departmentI, setDepartmentI] = useState('');
   const [{ disEmail, disPassword }, setDisabledState] = useState({ disEmail: false, disPassword: false });
 
   useEffect(() => {
     const setLineInfo = () => {
       if (typeof props.lineInfo !== 'undefined') {
         if (props.lineInfo.email !== '' && props.lineInfo.password !== '') {
-          let formInit = { ...validator.formElements };
-          formInit.fullname.value = props.lineInfo.fullname;
-          formInit.email.value = props.lineInfo.email;
-          formInit.password.value = props.lineInfo.password;
-          setValidator({ ...validator, formElements: formInit, formValid: true });
+          setFormdata({
+            ...formData,
+            fullname: props.lineInfo.fullname,
+            email: props.lineInfo.email,
+            password: props.lineInfo.password
+          });
           setDisabledState({ disEmail: true, disPassword: true });
         }
       }
@@ -81,48 +79,50 @@ const Register = (props) => {
     setLineInfo();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [props.lineInfo]);
-
+  useEffect(() => {
+    getChangwat();
+  }, []);
+  const [formData, setFormdata] = useState({
+    fullname: '',
+    position: '',
+    cid: '',
+    email: '',
+    mobile: '',
+    password: '',
+    changwat: '',
+    department: ''
+  });
   const [validator, setValidator] = useState({
     formElements: {
       fullname: {
-        type: 'text',
-        value: '',
         validator: { required: true, minLength: 5 },
         error: { status: false, message: '' }
       },
       position: {
-        type: 'text',
-        value: '',
         validator: { required: true, minLength: 5 },
         error: { status: false, message: '' }
       },
       cid: {
-        type: 'text',
-        value: '',
-        validator: { required: true, stringLength: 13 },
+        validator: { required: true, number: true, stringLength: 13, pattern: 'mod13' },
         error: { status: false, message: '' }
       },
       email: {
-        type: 'email',
-        value: '',
         validator: { required: true, pattern: 'email' },
         error: { status: false, message: '' }
       },
       password: {
-        type: 'text',
-        value: '',
         validator: { required: true, minLength: 5 },
         error: { status: false, message: '' }
       },
       mobile: {
-        type: 'text',
-        value: '',
         validator: { required: true, stringLength: 10 },
         error: { status: false, message: '' }
       },
       changwat: {
-        type: 'text',
-        value: '',
+        validator: { required: true },
+        error: { status: false, message: '' }
+      },
+      department: {
         validator: { required: true },
         error: { status: false, message: '' }
       }
@@ -130,61 +130,14 @@ const Register = (props) => {
     formValid: true
   });
   const onElementChange = (e) => {
-    const name = (typeof e.target !== 'undefined') ? e.target.name : e.name;
-    const value = (typeof e.target !== 'undefined') ? e.target.value : e.value;
-    let updateForm = { ...validator.formElements };
-    if (typeof updateForm[name] !== 'undefined') {
-      updateForm[name].value = value;
-      updateForm[name].touched = true;
-      const validatorObject = checkValidator(value, updateForm[name].validator);
-      updateForm[name].error = {
-        status: validatorObject.status,
-        message: validatorObject.message
-      }
-      let formStatus = true;
-      for (let name in updateForm) {
-        if (updateForm[name].validator.required === true) {
-          formStatus = (!updateForm[name].error.status) ? formStatus : false;
-        }
-      }
-      setValidator({ ...validator, formElements: updateForm, formValid: formStatus });
+    let result = validation(validator, e);
+    if (typeof result !== 'undefined') {
+      const name = (typeof e.target !== 'undefined') ? e.target.name : e.key;
+      const value = (typeof e.target !== 'undefined') ? e.target.value : e.val;
+      setFormdata({ ...formData, [name]: value });
+      setValidator({ ...validator, formElements: result.formElements, formValid: result.formValid });
     }
   }
-  const checkValidator = (value, rule) => {
-    let valid = true;
-    let message = '';
-    // ห้ามว่าง
-    if (rule.required) {
-      if (value.trim().length === 0) {
-        valid = false;
-        message = 'กรุณากรอกข้อมูล';
-      }
-    }
-    // ความยาวต้องเท่ากับ
-    if (typeof rule.stringLength !== 'undefined' && value.length !== rule.stringLength && valid) {
-      valid = false;
-      message = `ความยาว ${rule.stringLength} ตัวอักษร`;
-    }
-    // ความยาวอย่างน้อย
-    if (typeof rule.minLength !== 'undefined' && value.length < rule.minLength && valid) {
-      valid = false;
-      message = `กรุณากรอกข้อมูลอยางน้อย ${rule.minLength} ตัวอักษร`;
-    }
-    // ความยาวต้องไม่มากกว่า
-    if (typeof rule.maxLength !== 'undefined' && value.length > rule.maxLength && valid) {
-      valid = false;
-      message = `กรุณากรอกข้อมูลไม่เกิน ${rule.maxLength} ตัวอักษร`;
-    }
-    // รูปแบบอีเมลล์
-    if (typeof rule.pattern !== 'undefined' && rule.pattern === 'email' && valid) {
-      if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) === false) {
-        valid = false;
-        message = `อีเมลล์ไม่ถูกต้อง`;
-      }
-    }
-    return { status: !valid, message: message };
-  }
-
   const getChangwat = async () => {
     let response = await getAll({ filter: { "fields": { "changwatname": "true" }, "where": { "zonecode": "08" } } }, 'cchangwats');
     setChangwats(response.data);
@@ -193,10 +146,58 @@ const Register = (props) => {
     let response = await getAll({ filter: { "fields": { "hos_id": "true", "hos_name": "true", "hos_fullname": "true" }, "where": { "province_name": cw } } }, 'hospitals');
     setDepartments(response.data);
   }
-  useEffect(() => {
-    getChangwat();
-  }, []);
-
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    // Validate all element in form 
+    if (validator.formValid) {
+      Object.entries(formData).forEach(([k, v]) => {
+        onElementChange({ key: k, val: v });
+      })
+    }
+    let formStatus = true;
+    let form = { ...validator.formElements };
+    for (let name in form) {
+      if (form[name].validator.required === true) {
+        formStatus = (!form[name].error.status) ? formStatus : false;
+      }
+    }
+    if (formStatus) {
+      setValidator({ ...validator, formValid: false });
+      register(formData).then((response) => {
+        if (response.status === 200) {
+          // alert("ลงทะเบียนสำเร็จ รอผู้ดูแลระบบอนุมัติการใช้งาน");
+          customDialog({
+            title: 'ผลการลงทะเบียน',
+            contentWidth: '400px',
+            content: <span style={{ color: "green" }}>ลงทะเบียนสำเร็จ รอผู้ดูแลระบบอนุมัติการใช้งาน</span>,
+            onClick: () => { closeDialog(); redirect.push("/login"); }
+          });
+        } else if (response.status === 422) {
+          // alert('อีเมลล์นี้ถูกใช้ลงทะเบียนไปแล้ว กรุณาใช้อีเมลล์อื่น');
+          // setValidator({ ...validator, formValid: true });
+          customDialog({
+            title: 'ผลการลงทะเบียน',
+            contentWidth: '400px',
+            content: <span style={{ color: "red" }}>อีเมลล์นี้ถูกใช้ลงทะเบียนไปแล้ว กรุณาใช้อีเมลล์อื่น</span>,
+            onClick: () => { setValidator({ ...validator, formValid: true }); closeDialog(); }
+          });
+        }
+      });
+    }
+  };
+  const customDialog = (data) => {
+    openDialog({
+      children: (
+        <>
+          <DialogTitle style={{ padding: '30px 24px 0 24px' }}>{data.title}</DialogTitle>
+          <DialogContent style={{ width: data.contentWidth, padding: '24px', textAlign: 'center' }}>{data.content}</DialogContent>
+          <DialogActions style={{ padding: '0 24px 24px 24px' }}>
+            <Button color="primary" onClick={data.onClick}>ปิด</Button>
+          </DialogActions>
+        </>
+      )
+    });
+  };
   const registerForm = () => {
     return < div >
       <div className="form-group">
@@ -208,7 +209,7 @@ const Register = (props) => {
           size="small"
           variant="outlined"
           onChange={(e) => onElementChange(e)}
-          value={validator.formElements.fullname.value}
+          value={formData.fullname}
           error={validator.formElements.fullname.error.status}
           helperText={validator.formElements.fullname.error.message}
         />
@@ -222,7 +223,7 @@ const Register = (props) => {
           size="small"
           variant="outlined"
           onChange={(e) => onElementChange(e)}
-          value={validator.formElements.position.value}
+          value={formData.position}
           error={validator.formElements.position.error.status}
           helperText={validator.formElements.position.error.message}
         />
@@ -236,7 +237,7 @@ const Register = (props) => {
           size="small"
           variant="outlined"
           onChange={(e) => onElementChange(e)}
-          value={validator.formElements.cid.value}
+          value={formData.cid}
           error={validator.formElements.cid.error.status}
           helperText={validator.formElements.cid.error.message}
         />
@@ -250,7 +251,7 @@ const Register = (props) => {
           size="small"
           variant="outlined"
           onChange={(e) => onElementChange(e)}
-          value={validator.formElements.mobile.value}
+          value={formData.value}
           error={validator.formElements.mobile.error.status}
           helperText={validator.formElements.mobile.error.message}
         />
@@ -265,7 +266,7 @@ const Register = (props) => {
           variant="outlined"
           autoComplete='new-password'
           onChange={(e) => onElementChange(e)}
-          value={validator.formElements.email.value}
+          value={formData.email}
           error={validator.formElements.email.error.status}
           helperText={validator.formElements.email.error.message}
           disabled={disEmail}
@@ -281,7 +282,7 @@ const Register = (props) => {
           variant="outlined"
           autoComplete='new-password'
           onChange={(e) => onElementChange(e)}
-          value={validator.formElements.password.value}
+          value={formData.value}
           error={validator.formElements.password.error.status}
           helperText={validator.formElements.password.error.message}
           disabled={disPassword}
@@ -292,17 +293,23 @@ const Register = (props) => {
           id="changwat"
           size="small"
           fullWidth
-          required
           options={changwats}
           getOptionSelected={(option, value) => value.changwatname === option.changwatname}
           getOptionLabel={(option) => option.changwatname || ''}
           onChange={(e, newValue) => {
-            setChangwat((newValue) ? newValue.changwatname : '');
-            setDepartment('');
-            if (newValue !== null) { getDepartment(newValue.changwatname) }
+            let changwatName = (newValue != null) ? newValue.changwatname : '';
+            setFormdata({
+              ...formData,
+              changwat: changwatName,
+              department: ''
+            });
+            onElementChange({ key: 'changwat', val: changwatName });
+            if (changwatName !== '') { getDepartment(changwatName) }
           }}
           renderInput={(params) =>
             <TextField {...params}
+              id="changwat_texfield"
+              name="changwat"
               label="จังหวัด"
               variant="outlined"
               onChange={(e) => onElementChange(e)}
@@ -317,55 +324,31 @@ const Register = (props) => {
           id="department"
           size="small"
           fullWidth
-          required
           options={departments}
           getOptionSelected={(option, value) => {
             return value === option
           }}
           getOptionLabel={(option) => option.hos_name || ''}
-          value={department}
+          value={formData.department}
           onChange={(_, newValue) => {
             delete Object.assign(newValue, { 'hcode': newValue['hos_id'] })['hos_id'];
-            setDepartment((newValue !== null) ? newValue : '')
+            setFormdata({ ...formData, department: (newValue !== null) ? newValue : '' });
+            onElementChange({ key: 'department', val: (newValue !== null) ? newValue : '' });
           }}
-          renderInput={(params) => <TextField {...params} label="หน่วยงาน" variant="outlined" />}
+          renderInput={(params) => <TextField {...params}
+            label="หน่วยงาน"
+            variant="outlined"
+            onChange={(e) => onElementChange(e)}
+            error={validator.formElements.department.error.status}
+            helperText={validator.formElements.department.error.message}
+          />}
         />
       </div>
       <div className="form-group">
         <button className="btn btn-primary btn-block" disabled={!validator.formValid}>ลงทะเบียน</button>
       </div>
-    </div >
+    </div>
   }
-
-  const onFormSubmit = (event) => {
-    event.preventDefault();
-    Object.entries(event.target.elements).forEach(([name, input]) => {
-      if (input.type !== 'submit' && (input.type === 'text' || input.type === 'password')) {
-        onElementChange(input);
-      }
-    });
-    if (validator.formValid) {
-      let param = {
-        fullname: validator.formElements.fullname.value,
-        position: validator.formElements.position.value,
-        cid: validator.formElements.cid.value,
-        mobile: validator.formElements.mobile.value,
-        email: validator.formElements.email.value,
-        password: validator.formElements.password.value,
-        changwat: changwat,
-        department: department
-      }
-      AuthService.register(param).then(
-        (response) => {
-          if (response.status === 200) {
-            alert("ลงทะเบียนสำเร็จ รอผู้ดูแลระบบอนุมัติการใช้งาน");
-            redirect.push("/login");
-          }
-        },
-        (error) => { console.log(error) }
-      );
-    }
-  };
 
   return (
     <div className="col-md-12">
@@ -375,7 +358,6 @@ const Register = (props) => {
         </label>
         <Form className={classes.root} onSubmit={onFormSubmit} ref={form} autoComplete="new-password">
           {registerForm()}
-          <CheckButton style={{ display: "none" }} ref={checkBtn} />
         </Form>
       </div>
     </div>
