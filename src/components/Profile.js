955ba38a28325ecb85f453dce2bdc566c7935c72
keling <keling@windowslive.com>
2021-03-09 10:35:41 +0700

    Edit profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,52 +1,39 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import * as AuthService from "../services/auth.service";
+import { makeStyles  } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import useGlobal from "../store";
+import logo from "../images/logo192.png";
+
+const useStyles = makeStyles((theme) => ({
+  hide: { display: 'none' },
+  avatarLarge: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+}));
+
 const Profile = () => {
+  const [globalState] = useGlobal();
+  const classes = useStyles();
   const currentUser = AuthService.getCurrentUser();
-  const [authorizes, setAuthorizes] = useState([]);
-  // useEffect(() => {
-  //   if (currentUser) {
-  //     retrieveAuthorizes();
-  //   }
-  // }, []);
-
-  // const retrieveAuthorizes = () => {
-  //   // setAuthorizes(localStorage.getItem("auth"));
-  //   AuthService.getAuthorize(currentUser)
-  //     .then(response => {
-  //       localStorage.setItem("auth", JSON.stringify(response.data));
-  //       // setAuthorizes(response.data);
-  //       setAuthorizes(JSON.parse(localStorage.getItem("auth")));
-  //       // console.log(response.data);
-  //     })
-  //     .catch(e => {
-  //       console.log(e);
-  //     });
-  // };
-
+  
   return (
     <div className="container">
-      <header className="jumbotron">
-        <h3>
-          <strong>{currentUser.username}</strong> Profile
-        </h3>
-      </header>
-      <p>
-        <strong>Token:</strong> {currentUser.id.substring(0, 20)} ...{" "}
-        {currentUser.id.substr(currentUser.id.length - 20)}
-      </p>
-      <p>
-        <strong>Id:</strong> {currentUser.userId}
-      </p>
-      <p>
-        <strong>Email:</strong> {currentUser.user.email}
-      </p>
-      <strong>Authorities:</strong>
-      {authorizes.map((myData, i) => (
-        myData.resource_role.map((resource, ii) => (
-          <li key={ii}>{resource}</li>
-        ))
-      ))}
+      <h4>Profile</h4>
+      <br/>
+      {(!globalState.isLineLogin)?(
+        <Avatar className={classes.avatarLarge} src={logo} />
+      ):(
+        <Avatar className={classes.avatarLarge} src={(Object.keys(globalState.currentUser).length!==0)?globalState.currentUser.user.picture: ''} />
+      )}<br/>
+      <p><strong>ID :</strong> {currentUser.userId}</p>
+      <p><strong>ชื่อ - สกุล :</strong> {currentUser.user.fullname}</p>
+      <p><strong>ตำแหน่ง :</strong> {currentUser.user.position}</p>
+      <p><strong>เบอร์โทร :</strong> {currentUser.user.mobile}</p>
+      <p><strong>Email :</strong> {currentUser.user.email}</p>
+      <p><strong>Role : </strong> {currentUser.user.role}</p>
     </div>
   );
 };
