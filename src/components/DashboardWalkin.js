147d89afca6c3502923db1f06b2b5ff0475d70a8
keling <keling@windowslive.com>
2021-06-04 17:26:26 +0700

    up dashboard

--- src/components/DashboardWalkin.js
+++ src/components/DashboardWalkin.js
@@ -10,32 +10,37 @@ import {
 const DashboardWalkin = () => {
     const [data, setData] = useState(null);
 
-    useEffect(() => {
-        const getData = async () => {
-            let xParams = {
-                filter: {
-                  limit: 5,
-                  include: ["hospital"],
-                  where: {
-                    
-                  },
-                }
-            };
+    const getData = async () => {
+        // let andQuery = [];
+        // andQuery.push({ hcode: sentHcode });
+        let xParams = {
+            filter: {
+              limit: 5,
+              include: ["hospital"],
+              where: {
+                "walkin_pttype": ["wi", "WI", "wo", "WO"]
+              }
+            }
+        };
 
-            let response = await getAll(xParams, 'interventions');
-            if (response.status === 200) {
-                if (response.data) {
-                    if (response.data.length > 0) {
-                        setData(response.data);
-                        console.log(response.data);
-                        // setOpenBackdrop(false);
-                    } else {
-                        // setOpenBackdrop(false);
-                    }
+        let response = await getAll(xParams, 'interventions');
+        if (response.status === 200) {
+            if (response.data) {
+                if (response.data.length > 0) {
+                    setData(response.data);
+                    console.log(response.data);
+                    // setOpenBackdrop(false);
+                } else {
+                    // setOpenBackdrop(false);
                 }
             }
-            // console.log(xParams);
+        } else {
+            console.log("error");
         }
+        // console.log(xParams);
+    }
+
+    useEffect(() => {
         getData();
     }, []);
 
