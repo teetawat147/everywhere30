420e4221bc2c282bd92375edf61ad33b9e84314c
keling <keling@windowslive.com>
2021-06-04 16:41:38 +0700

    up dashboard

--- src/components/DashboardWalkin.js
+++ src/components/DashboardWalkin.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { getAll, getCount } from "../services/UniversalAPI";
+
 import {
     Button as MuiButton,
     Card as MuiCard,
@@ -6,11 +8,35 @@ import {
   } from "@material-ui/core";
 
 const DashboardWalkin = () => {
+    const [data, setData] = useState(null);
+
     useEffect(() => {
-        const getCountPerson = () => {
-            console.log("OK");
+        const getData = async () => {
+            let xParams = {
+                filter: {
+                  limit: 5,
+                  include: ["hospital"],
+                  where: {
+                    
+                  },
+                }
+            };
+
+            let response = await getAll(xParams, 'interventions');
+            if (response.status === 200) {
+                if (response.data) {
+                    if (response.data.length > 0) {
+                        setData(response.data);
+                        console.log(response.data);
+                        // setOpenBackdrop(false);
+                    } else {
+                        // setOpenBackdrop(false);
+                    }
+                }
+            }
+            // console.log(xParams);
         }
-        getCountPerson();
+        getData();
     }, []);
 
     return (
