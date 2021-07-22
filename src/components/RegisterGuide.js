49715e7883f149a72ac0667cec143c74a62ff664
keling <keling@windowslive.com>
2021-07-22 09:40:21 +0700

    up link

--- src/components/RegisterGuide.js
+++ src/components/RegisterGuide.js
@@ -8,7 +8,7 @@ class RegisterGuide extends Component {
             <div>
                 <InfoIcon style={{ color: green[500] }}/>&nbsp;<b>คำแนะนำ</b><br/>
                 1. ลงทะเบียนผู้ใช้งานในเว็บไซต์ R8anywhere ได้ที่ <a href="https://cloud1.r8way.moph.go.th/r8anywhere/register">https://cloud1.r8way.moph.go.th/r8anywhere/register</a> (สามารถลงทะเบียนด้วย Line account ได้)<br/>
-                2. กรอกข้อมูลในแบบฟอร์ม (เอกสาร) ลงทะเบียนเข้าใช้งานระบบสารสนเทศ เพื่อเป็นการยืนยันว่าเป็นบุคลากรในหน่วยงานสาธารณสุข (ลงนามรับรองด้วยหัวหน้าหน่วยงานหรือผู้บังคับบัญชา) Download แบบฟอร์มได้ที่ <a href="https://r8way.moph.go.th/r8way/view_publicize.php?id=1642" rel="noreferrer" target="_blank">Click ที่นี่</a><br/>
+                2. กรอกข้อมูลในแบบฟอร์ม (เอกสาร) ลงทะเบียนเข้าใช้งานระบบสารสนเทศ เพื่อเป็นการยืนยันว่าเป็นบุคลากรในหน่วยงานสาธารณสุข (ลงนามรับรองด้วยหัวหน้าหน่วยงานหรือผู้บังคับบัญชา) Download แบบฟอร์มได้ที่ <a href="https://drive.google.com/drive/folders/1oNzlwBUcKMGZjC5_kPTxvROo_ePV5Yau?usp=sharing" rel="noreferrer" target="_blank">Click ที่นี่</a><br/>
                 3. ส่งแบบฟอร์มลงทะเบียนกลับมาที่เขตสุขภาพที่ 8 ทางอีเมล์ contact.r8way@gmail.com<br/>
                 4. ผู้ดูแลระบบ จะยืนยันสิทธิ์การใช้งานให้กับผู้ลงทะเบียนที่มีชื่อ-สกุล อีเมล์ ตรงกับที่ลงทะเบียนในแบบฟอร์มเอกสารเท่านั้น<br/>
             </div>
